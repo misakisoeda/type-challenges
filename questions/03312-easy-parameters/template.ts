370fa40c421a9bcc56af1cdb7dafa335648dee9e
misakisoeda <misaki.soeda.29@gmail.com>
2025-04-06 14:24:24 +0900

    add: no.3312

--- questions/03312-easy-parameters/template.ts
+++ questions/03312-easy-parameters/template.ts
@@ -1,1 +1,13 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T> = T extends (...args: infer F) => any ? F : never
+
+import type { Equal, Expect } from '@type-challenges/utils'
+
+function foo(arg1: string, arg2: number): void {}
+function bar(arg1: boolean, arg2: { a: 'A' }): void {}
+function baz(): void {}
+
+type cases = [
+  Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
+  Expect<Equal<MyParameters<typeof bar>, [boolean, { a: 'A' }]>>,
+  Expect<Equal<MyParameters<typeof baz>, []>>,
+]
